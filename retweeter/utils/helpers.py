21eca3da35663fe284d393f472c7d13a60df780b
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2011-12-16 18:42:41 +0530

    Fixed render_page and logged_in_page.html for msgs and errors.

--- retweeter/utils/helpers.py
+++ retweeter/utils/helpers.py
@@ -4,4 +4,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def render_page(template_name, data, request):
+	data['error'] = request.GET.get('error')
+	data['msg'] = request.GET.get('msg')
+
 	return render_to_response(template_name, data, context_instance=RequestContext(request))
