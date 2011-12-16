df8cd04c50279bdd2dfd05f64fb79fda1a26ef1a
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2011-12-16 21:14:32 +0530

    Superuser can now set the twitter username of the organisation.

--- retweeter/utils/helpers.py
+++ retweeter/utils/helpers.py
@@ -2,9 +2,12 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.conf import settings
 
 def render_page(template_name, data, request):
 	data['error'] = request.GET.get('error')
 	data['msg'] = request.GET.get('msg')
+	data['globals'] = {}
+	data['globals']['twitter_account'] = settings.TWITTER_ACCOUNT
 
 	return render_to_response(template_name, data, context_instance=RequestContext(request))
